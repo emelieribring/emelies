fa4d9f396d59459e91153c5ef9cadc4965aa1d37
emelieribring <emelieribring@hotmail.com>
2024-04-18 12:31:29 +0200

    construction

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './home.css';
+import 'animate.css';
+import workpic from '../../images/work-98936_1280.png';
+
+
+export default function Home() {
+  return (
+    <div className='home'>
+        <div className='big-cloud animate__animated animate__fadeInDownBig'>
+            <div className='line'></div>
+            <img className='workpic' src={workpic} alt="" />
+        <p className='text'>Under construction</p>
+            <div className="cloudball-one"></div>
+            <div className='cloud'>
+            <div className="cloud-box">
+                    <div className='clouds-top'></div>
+                    <div className='clouds-top'></div>
+                    <div className='clouds-top'></div>
+                </div>
+                <div className="cloud-box">
+                    <div className='clouds'></div>
+                    <div className='clouds'></div>
+                    <div className='clouds'></div>
+                </div>
+            </div>
+            <div className="cloudball-two"></div>
+        </div>
+    </div>
+  )
+}
